2b2e44e016d83d7616e95e02d6f89948bd132a8f
james479 <james.e.glover57@gmail.com>
2021-03-17 14:49:44 -0500

    inital commit - brought template to project

---
+++ ScheduleAdminTool/ScheduleAdminTool.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScheduleAdminTool.UI.MVC.MvcApplication" Language="C#" %>
---
+++ ScheduleAdminTool/ScheduleAdminTool.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScheduleAdminTool.UI.MVC.Startup))]
+namespace ScheduleAdminTool.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
